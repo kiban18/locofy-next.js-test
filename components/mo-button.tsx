a9a1b4edf5d5c395e7aa40f831d2cffd0c6e77c9
kiban18 <kiban18>
2024-04-30 07:58:03 +0000

    Pushed from Locofy Builder

--- components/mo-button.tsx
+++ components/mo-button.tsx
@@ -6,7 +6,7 @@ export type MoButtonType = {
 
 const MoButton: NextPage<MoButtonType> = ({ title }) => {
   return (
-    <button className="cursor-pointer py-px px-0 bg-[transparent] w-[300px] box-border h-[42px] flex flex-col items-center justify-start max-w-[306px] border-[1px] border-solid border-whitesmoke">
+    <button className="cursor-pointer py-px px-0 bg-[transparent] w-[300px] box-border h-[42px] flex flex-col items-center justify-start max-w-[306px] border-[1px] border-solid border-tailwind-v332-slate-50">
       <div className="self-stretch relative text-sm leading-[40px] font-kasprnet-noto-sans-kr-demilight-13 text-tailwind-v332-white text-center">
         {title}
       </div>
